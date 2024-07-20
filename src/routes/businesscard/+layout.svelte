3a8bce5cb27f365b1e3753e4ea578aa6e785b512
Jeff Ng <50jeffng@gmail.com>
2024-07-20 09:36:00 +0800

    feat: add business card and change namecard link to that

---
+++ src/routes/businesscard/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import '../../app.css';
+</script>
+
+<main class="bg-base-100">
+	<slot></slot>
+</main>
+
+<style lang="postcss">
+	:global(body) {
+		@apply bg-primary;
+		@apply font-sans;
+	}
+</style>
--- src/routes/businesscard/+page.svelte
+++ src/routes/businesscard/+page.svelte
@@ -1,1 +1,30 @@
-<span>you shouldn't be here.</span>
+<script lang="ts">
+	import { constants } from '$lib';
+	import IconArrowLeft from '~icons/mdi/arrow-left';
+	import IconEmail from '~icons/mdi/email';
+	import IconGithub from '~icons/mdi/github';
+</script>
+
+<div class="flex flex-col h-screen items-center justify-center gap-8">
+	<a class="flex" href="/">
+		<IconArrowLeft class="h-6 w-6 shrink-0 mr-2" /> to homepage
+	</a>
+	<div class="card bg-primary w-[20em] md:w-[35em] lg:w-[40em] md:h-96 shadow-xl">
+		<div class="grid grid-cols-2 py-8 px-16 gap-32 h-96 whitespace-nowrap">
+			<div class="flex flex-col">
+				<h1 class="my-4 text-xl font-bold">Jeff Ng</h1>
+				<p>Software Engineer</p>
+			</div>
+			<div class="flex flex-col items-end justify-end">
+				<div class="mt-8 grid grid-rows-none gap-2">
+					<a class="btn btn-secondary" href="https://github.com/jeffng50"
+						><IconGithub class="hidden md:block h-6 w-6 shrink-0" />Github</a
+					>
+					<a class="btn btn-secondary" href={`mailto:${constants.email}`}
+						><IconEmail class="hidden md:block h-6 w-6 shrink-0" />Mail me
+					</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
