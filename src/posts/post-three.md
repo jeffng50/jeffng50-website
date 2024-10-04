5f429775b26e506f7641468b5ce39b527f837e25
Jeff Ng <50jeffng@gmail.com>
2024-10-04 23:44:51 +0800

    test: change test values

---
+++ src/posts/post-three.md
@@ -0,0 +1,14 @@
+---
+title: Post Three
+description: This is the third post
+createdDate: 2024-07-16
+modifiedDate: 2024-07-23
+tags:
+  - personal
+---
+
+Hello, I am _Post Three._
+
+**Nice to meet you!** This is for testing purposes
+
+![image](/images/jeef.arts_logo.png)
--- src/routes/(main)/blog/+page.ts
+++ src/routes/(main)/blog/+page.ts
@@ -1,12 +1,6 @@
-import { get } from 'svelte/store';
-
-import { isPersonal } from '$lib';
-
 export const load = async ({ fetch }) => {
 	const response = await fetch(`/api/posts`);
-	const allPosts = await response.json();
-
-	const posts = allPosts;
+	const posts = await response.json();
 
 	return {
 		posts
