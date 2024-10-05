770e53ce8fc7bef232357f474a64ea725d43b80d
Jeff Ng <50jeffng@gmail.com>
2024-10-05 09:52:29 +0800

    feat: set fallback 404 html

--- svelte.config.js
+++ svelte.config.js
@@ -24,7 +24,7 @@ const config = {
 			// these options are set automatically — see below
 			pages: 'build',
 			assets: 'build',
-			fallback: undefined,
+			fallback: 'src/404.html',
 			precompress: false,
 			strict: true
 		})
