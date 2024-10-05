d97279d89f8c4a496a0f8d8958c49f6245786b7a
Jeff Ng <50jeffng@gmail.com>
2024-10-05 10:07:50 +0800

    fix: revert

--- src/routes/(main)/blog/[slug]/+page.svelte
+++ src/routes/(main)/blog/[slug]/+page.svelte
@@ -8,27 +8,23 @@
 </script>
 
 <div class="flex items-center justify-center">
-	{#if data}
-		<article
-			class="prose lg:prose-xl prose-a:text-blue-600 hover:prose-a:text-blue-800 visited:prose-a:text-purple-600 container p-6"
-		>
-			<h1>{data.title}</h1>
-			{#each data.tags as tag}
-				<a href={`/blog/tags/${tag}`} class="badge text-black bg-[#e5e7eb] mr-2">
-					{tag}
-				</a>
-			{/each}
-			<p>Published: {new Date(data.createdDate).toLocaleDateString(undefined, dateOptions)}</p>
-			{#if data.modifiedDate}
-				<p>
-					Last Modified: {new Date(data.modifiedDate).toLocaleDateString(undefined, dateOptions)}
-				</p>
-			{/if}
-			<div class="divider"></div>
+	<article
+		class="prose lg:prose-xl prose-a:text-blue-600 hover:prose-a:text-blue-800 visited:prose-a:text-purple-600 container p-6"
+	>
+		<h1>{data.title}</h1>
+		{#each data.tags as tag}
+			<a href={`/blog/tags/${tag}`} class="badge text-black bg-[#e5e7eb] mr-2">
+				{tag}
+			</a>
+		{/each}
+		<p>Published: {new Date(data.createdDate).toLocaleDateString(undefined, dateOptions)}</p>
+		{#if data.modifiedDate}
+			<p>
+				Last Modified: {new Date(data.modifiedDate).toLocaleDateString(undefined, dateOptions)}
+			</p>
+		{/if}
+		<div class="divider"></div>
 
-			<svelte:component this={data.content} />
-		</article>
-	{:else}
-		<p>error 404</p>
-	{/if}
+		<svelte:component this={data.content} />
+	</article>
 </div>
